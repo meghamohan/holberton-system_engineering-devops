17f571c56dd236578f7be00e4d116d4fd0ec38df
meghamohan <meghamohan1989@gmail.com>
2017-09-19 19:56:42 +0000

    puppet formating fixes

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # increased number of files that can be opened
 exec { 'limit on open files':
     path	=> '/bin',
-    command	=> "sed -i 's/15/1001' /etc/default/nginx; /etc/init.d/nginx restart"
+    command	=> "sed -i 's/15/10000/g' /etc/default/nginx; /etc/init.d/nginx restart"
 }
