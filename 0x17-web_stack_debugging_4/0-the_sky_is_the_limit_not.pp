6699943fc1b4a816c644621b80aad40cd4fc698d
meghamohan <meghamohan1989@gmail.com>
2017-09-20 16:54:37 +0000

    fixing path

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # increased number of files that can be opened
-exec { 'limit on open files':
-    path	=> '/bin',
-    command	=> "sed -i 's/15/10000/g' /etc/default/nginx; /etc/init.d/nginx restart"
+exec { 'increase limit on open files':
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/15/15000/g' /etc/default/nginx; sudo service nginx restart;"
 }
